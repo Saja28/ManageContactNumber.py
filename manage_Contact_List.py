c9641aeb234b3d27de2b814f2c084806e54fab44
Saja28 <116615043+Saja28@users.noreply.github.com>
2025-01-09 21:44:55 -0500

    Update manage_Contact_List.py

--- manage_Contact_List.py
+++ manage_Contact_List.py
@@ -1,30 +1,24 @@
 import json
-
 class ContactNumber:
     def __init__ (self, name, number, hobby):
         self.name= name
         self.number= number
         self.hobby = hobby
     def print_info (self) :
-         print(f" {self.name}  {self.number} {self.hobby}")
-    
+         print(f" {self.name}  {self.number} {self.hobby}")    
     def save_to_json(self , filename):
         my_contact = { 'name' : self.name, 'number':self.number, 'hobby': self.hobby}
         with open(filename,'w' ) as f :
-            f.write(json.dumps(my_contact, indent=2))
+            f.write(JSON.dumps(my_contact, indent=2))
     def load_from_json(self, filename): 
         with open(filename,'r') as f:
-            data= json.loads(f.read())
-
+            data= JSON.loads(f.read())
         self.name = data['name']
         self.number = data['number']
         self.hobby = data['hobby']
-
-
 p1 = ContactNumber("mike" , 919990 ,"dancing" )
 p1.print_info()
 p1.save_to_json("mike.json")
-
 p2 = ContactNumber("Ali" , 91908656 , "none")
 p2.print_info()
 p2.save_to_json("Ali.json")
